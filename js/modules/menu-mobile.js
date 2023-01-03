8cda9eee096824136a46bc610ff614a2c1d7976d
Felippe Vieira <felippe_nvieira@hotmail.com>
2023-01-03 07:49:22 -0300

    menu-mobile refactor

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -1,21 +1,36 @@
 import outsideClick from "./outsideClick.js";
 
-export default function initMenuMobile() {
-  const btnMenu = document.querySelector('[data-menu="button"]');
-  const menuList = document.querySelector('[data-menu="list"]');
-  const eventos = ["click", "touchstart"];
+export default class MenuMobile {
+  constructor(btnMenu, menuList, events) {
+    this.btnMenu = document.querySelector(btnMenu);
+    this.menuList = document.querySelector(menuList);
+    this.activeClass = "active";
 
-  function openMenu() {
-    menuList.classList.add("active");
-    btnMenu.classList.add("active");
-    outsideClick(menuList, eventos, () => {
-      menuList.classList.remove("active");
-      btnMenu.classList.remove("active");
+    if (events === undefined) this.events = ["click", "touchstart"];
+    else this.events = events;
+
+    this.openMenu = this.openMenu.bind(this);
+  }
+
+  openMenu() {
+    this.menuList.classList.add(this.activeClass);
+    this.btnMenu.classList.add(this.activeClass);
+    outsideClick(this.menuList, this.events, () => {
+      this.menuList.classList.remove(this.activeClass);
+      this.btnMenu.classList.remove(this.activeClass);
     });
   }
-  if (btnMenu) {
-    eventos.forEach((event) => {
-      btnMenu.addEventListener(event, openMenu);
-    });
+
+  addMenuMobileEvents() {
+    this.events.forEach((event) =>
+      this.btnMenu.addEventListener(event, this.openMenu)
+    );
+  }
+
+  init() {
+    if (this.btnMenu && this.menuList) {
+      this.addMenuMobileEvents();
+    }
+    return this;
   }
 }
--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@ import ScrollSuave from "./modules/scroll-suave.js";
 import ScrollAnima from "./modules/scroll-anima.js";
 import Modal from "./modules/modal.js";
 import Tooltip from "./modules/tooltip.js";
-import initDropDownMenu from "./modules/menuDropDown.js";
-import initMenuMobile from "./modules/menu-mobile.js";
+import DropDownMenu from "./modules/dropdown-menu.js";
+import MenuMobile from "./modules/menu-mobile.js";
 import initFuncionamento from "./modules/funcionamento.js";
 import fetchAnimais from "./modules/fetch-animais.js";
 import fetchBitcoin from "./modules/fetch-bitcoin.js";
@@ -35,9 +35,13 @@ tooltip.init();
 const scrollAnima = new ScrollAnima("[data-scroll]");
 scrollAnima.init();
 
+const dropdownMenu = new DropDownMenu("[data-dropdown]");
+dropdownMenu.init();
+
+const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
+menuMobile.init();
+
 fetchAnimais("../../animaisAPI.json", ".numeros-grid");
 fetchBitcoin(".btc-preco", "https://blockchain.info/ticker");
 
-initDropDownMenu();
-initMenuMobile();
 initFuncionamento();
